5eb4aec5fcca49a2151d5d32973c6da57e62ebe6
jeffnoehren <jeffrey@spotintst.com>
2017-10-18 13:21:43 -0700

    Adding python mysql example

---
+++ python-mysql-connection/README.md
@@ -0,0 +1,76 @@
+# Connect to MySQL Database and Insert a New Value
+
+This porject will allow you to create a connection to a MySQL database and either return all the values or given the right parameters will insert a new value into your table.
+
+### Prerequisites
+
+You will need to have the serverless framework installed, your Spotinst credentials set up and you will need MySQL database credentials, environment ID and account ID in order to run this project. 
+
+First you will want to install the serverless framework onto your local machine using the terminal command
+
+```
+npm install -g serverless
+```
+
+Once this has finished downloading you will have to set up your credentials to link your local machine projects to your Spotinst console. To do this please follow the directions listed [here](https://serverless.com/framework/docs/providers/spotinst/guide/credentials/).
+
+You will also need your environment ID for your function. This can be found on the Spotinst console under the Serverless Functions tab. Select the application you wish to deploy this funciton to and locate the environment that you wish to use. Copy the environment ID and save this for later use
+
+Additionally you will need to have your MySQL `host`, `username`, `database`, and `password` to gain access to your data table.
+
+The last thing you will need is your account ID which can be found in the Spotinst console under user setting. Save this for later use. 
+
+### Installing
+
+First you will need to fork this repository and set it up on your local machine. Then you will need to install the serverless-spotinst-functions plugin by running this command:
+
+```
+npm install
+```
+
+Then you will need to have the MySQL library installed into your project directory so you will need to run this command in the root directory for your project
+
+```
+pip install mysql-connector==2.1.4 -t /path/to/project-dir
+```
+
+Once this has been completed navigate to the handler.py file and input your MySQL credentials. Then you will need to navigate to the serverless.yml file and under environment add in the environment ID in the environment section.
+
+## Deployment
+
+Next you will want to deploy this function using this command:
+
+```
+sls deploy
+```
+
+The first time you run this command your new function will be created and linked to your Spotinst account under the environment that you specified. You can check this on the Spotinst Functions console. 
+
+## Testing
+
+To test if this is working use the command:
+
+```
+sls invoke -f SQLConnect
+```
+
+Or you can use the test feature on the Spoinst console. Either way the output to the console should be 'Success' if you have connected to your data table properly. If you look at the logs on the Spotinst console you will see all the contents of your data table. If you have entered the wrong credentials you will get an error.
+
+## Invoking 
+
+For practical use, every Spotinst function is given a unique URL that will run the function every time the HTTPS request is made. This function also has URL parameters that can be changed to enter in new rows to your data table. In order to do this you will need to add a new row you will need to add the parameter `number=1` to the URL. Additionally there are four fields that need to be entered as well and they are:
+
+	* `firstName`
+	* `lastName`
+	* `email`
+	* `instances`
+
+These feilds are specific to the data table and can be personalized to your project. 
+
+**To access URL parameters** simply add `querypram['{Parameter Name}']` into your code as needed.
+
+## Optional
+
+If you want you can set this function up to run on a schedule using the cron function in the serverless.yml file. Use the [cron function reference](https://crontab.guru/) for help on setting up your cron function. 
+
+
---
+++ python-mysql-connection/handler.py
@@ -0,0 +1,141 @@
+""" 
+This function will connect to your database and if you do not enter any URL parameters
+it will return with all the values in your data table. If you enter 'number=1' into
+the URL parameters then we will attempt to add a new row into the data table. First
+we will check that all the neccessary parameters are entered into the URL then check
+that the row does not already exsist in the data table. If both checks pass then 
+the new row will be inserted into the data table
+
+To personalize this function you will need to enter your own MySQL credentials as well
+as check the appropriate query paramneters that are needed for your data table throughout
+this function.
+"""
+import mysql.connector
+
+"""This function when called will print out all the values in the data table to the console
+Args:
+	cursor: This holds the connection to the database and will execute the query then 
+	        hold the result to be printed
+"""
+def getAllRows(cursor):
+	query = "SELECT * FROM customers"
+	print(query)
+	cursor.execute(query)
+
+	for element in cursor:
+		print(element) 
+
+"""
+This funcition takes in the query parameters and check that they are all completed.
+It will return False if all params are there and True if even one is missing.
+Here is where you should enter in your own data table columns that are required for 
+inserting a new row
+Args: 
+	queryparams: This is where the URL parameters live and are reference by name.
+Returns:
+	(Bool): Will return False if all parameters are set in the URL
+"""
+def checkParams(queryparams):
+	try:
+		queryparams['firstName']
+		queryparams['lastName']
+		queryparams['email']
+		queryparams['instances']
+	except:
+		return True
+	else:
+		return False
+"""
+This function will take in the connection to the DB and the array of parameters to check
+that the row that is being entered does not already exsist in the data table. If it does
+the function will return True, if not it will return False
+Args: 
+	queryparams: This is where the URL parameters live and are reference by name.
+         cursor: This holds the connection to the database and will execute the 
+                 query then hold the result to be printed
+Returns:   
+	(Bool): Will return True if the row exsist in the data table
+"""
+def checkInTable(queryparams, cursor):
+	testString = "first_name='" + queryparams['firstName'] + "' AND last_name='" + queryparams['lastName'] + "' AND email='" + queryparams['email'] + "'"
+	testQuery = "SELECT COUNT(*) FROM customers WHERE " + testString
+	cursor.execute(testQuery)
+	for element in cursor:
+		if(element[0]>0):
+			return True
+	return False
+
+"""
+
+Args:
+	queryparams: This is where the URL parameters live and are reference by name.
+         cursor: This holds the connection to the database and will execute the 
+                 query then hold the result to be printed
+     connection: This connection to the DB allows you to write the new value to the table
+"""
+def insertNewRow(queryparams, cursor, connection):
+	queryString = "( '" + queryparams['firstName'] + "', '" + queryparams['lastName'] + "', '" + queryparams['email'] + "', '" + queryparams['instances'] + "' )"
+	query = "INSERT INTO customers (first_name, last_name, email, instances) VALUES"+queryString
+	print(query)
+	cursor.execute(query)
+	connection.commit()
+
+"""
+This is the main function that is executed by the Serverless framework. It takes in
+the URL parameters and return a serverless object that either return status code 400
+if there was an error. Here is where you will need to configure your MySQL database
+connection
+Args:
+	                arg: Serverless object that contains the URL parameters
+Returns: 
+	(Serverless Object): Serverless object that will have a status code 400 if there
+	                     was an error executing the code or 200 if not
+"""	
+def main(args):
+	#configuring connection to Database
+	config = {
+		'user':{Your Username}, 
+		'password':{Your Password}, 
+		'host':{Your Host Name},
+		'database':{Your Database Name}
+	}
+	#creating a connection to write to database and a cursor to read from database
+	connection = mysql.connector.connect(**config)
+	cursor = connection.cursor(buffered=True)
+
+	#getting URL parameters
+	queryparams = args.get("query", {})
+	# Checking if there is 'number=1' in the URL params
+	try:
+		queryparams['number']
+	except:
+		# Getting all rows if there is no 'number=1'
+		getAllRows(cursor)
+	else:
+		#checking that all the parameters are met
+		if checkParams(queryparams):
+			return {
+				'statusCode': 400,
+				'body': 'Parameters not defined',
+				'headers': {"Content-Type": "application/json"}
+			}
+		#checking that the row being inserted does not exist in the table
+		elif checkInTable(queryparams, cursor):
+			return {
+				'statusCode': 400,
+				'body': 'You have already entered this row',
+				'headers': {"Content-Type": "application/json"}
+			}
+		#inserting the new row if all the parameters are met
+		else:
+			insertNewRow(queryparams, cursor, connection)
+
+	#closing both connections to the database
+	cursor.close()
+	connection.close()
+
+	return {
+	    'statusCode': 200,
+	    'body': 'Success',
+	    'headers': {"Content-Type": "application/json"}
+	}
